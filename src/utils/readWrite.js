09965fc7a19dc0de0513b0d624f5cd6e728af45e
Rafael Soares <r.soares2015@outlook.com>
2022-12-15 16:23:37 -0300

    refactor: Refatoracao das funcoes writer e update

--- src/utils/readWrite.js
+++ src/utils/readWrite.js
@@ -16,7 +16,11 @@ const writer = async (content) => {
     const talkers = await reader();
     const id = talkers[talkers.length - 1].id + 1;
     const updatedTalkers = [...talkers, { id, ...content }];
-    await fs.writeFile(path, JSON.stringify(updatedTalkers));
+    try {
+        await fs.writeFile(path, JSON.stringify(updatedTalkers));
+    } catch (e) {
+        console.error(e.message);
+    }
 };
 
 const update = async (talker, id) => {
@@ -26,8 +30,12 @@ const update = async (talker, id) => {
     talkers[targetTalkerIndex].name = name;
     talkers[targetTalkerIndex].age = age;
     talkers[targetTalkerIndex].talk = { ...talk };
-    await fs.writeFile(path, JSON.stringify(talkers));
-    return talkers[targetTalkerIndex];
+    try {
+        await fs.writeFile(path, JSON.stringify(talkers));
+        return talkers[targetTalkerIndex];
+    } catch (e) {
+        console.error(e.message);
+    }
 };
 
 module.exports = {
