758916464b5e9b67ed150865ee5829d93c705d97
Rafael Soares <r.soares2015@outlook.com>
2022-12-15 16:15:57 -0300

    refactor: Refatoracao do middleware checkRate (6º requisito)

--- src/middlewares/checkRate.js
+++ src/middlewares/checkRate.js
@@ -1,11 +1,14 @@
+const MIN_RATE = 1;
+const MAX_RATE = 5;
+
+const checkValue = (rate) => (!Number.isInteger(rate) || rate < MIN_RATE || rate > MAX_RATE);
+
 module.exports = (req, res, next) => {
     const { talk: { rate } } = req.body;
-    const MIN_RATE = 1;
-    const MAX_RATE = 5;
-    if (!rate) {
+    if (!rate && rate !== 0) {
         return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
     }
-    if (!Number.isInteger(rate) || rate < MIN_RATE || rate > MAX_RATE) {
+    if (checkValue(rate)) {
         return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
     }
 
