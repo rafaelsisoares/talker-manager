fa93da9a7bdf26350c664aad70047fa2034aa73c
Rafael Soares <r.soares2015@outlook.com>
2022-12-14 22:52:05 -0300

    fix: Corrigida a mensagem de erro no checkName (5º requisito)

--- src/middlewares/checkName.js
+++ src/middlewares/checkName.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
     const { name } = req.body;
     const MIN_NAME_LENGTH = 3;
     if (!name) {
-        return res.status(400).json({ message: 'O campo "name é obrigatório"' });
+        return res.status(400).json({ message: 'O campo "name" é obrigatório' });
     }
     if (name.length < MIN_NAME_LENGTH) {
         return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
